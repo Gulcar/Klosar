5458a158098f6f3e22d77b683755e8d4ea4d634e
Gulcar <weplaymods@gmail.com>
2023-04-17 20:02:20 +0200

    dela odpreti okno

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,10 +13,25 @@ int main()
     std::cout << "Pozdravljen svet!\n";
 
     glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1600, 900, "Klosar", 0, 0);
+    glfwMakeContextCurrent(window);
 
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glViewport(0, 0, 1600, 900);
 
-    glm::vec3 a;
-    a.x = 5;
-    std::cout << a.x;
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
 }
