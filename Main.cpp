0f78a8ed793f698d1fe99b34460f28774bab4bba
Gulcar <weplaymods@gmail.com>
2023-04-17 16:32:36 +0200

    test 5

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 //Jakob Jeraj R2D Jakob je bil tukaj in je pisal v to datoteko.
 //DANES SEM JEDEL SIR
+//jakob je bil tretjic tukaj samo za test
 //jaz pa denes nisem jedel sira
 
 #include <iostream>
