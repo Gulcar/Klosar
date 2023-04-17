225bbccbf44cf0f4ffba37448e773b3718f82628
Gulcar <weplaymods@gmail.com>
2023-04-17 16:29:19 +0200

    pozdravljen svet

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,10 @@
 //Jakob Jeraj R2D Jakob je bil tukaj in je pisal v to datoteko.
 //DANES SEM JEDEL SIR
 //jaz pa denes nisem jedel sira
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Pozdravljen svet!\n";
+}
